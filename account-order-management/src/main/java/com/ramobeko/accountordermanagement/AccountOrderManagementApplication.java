c00e39b2f30dd817b7f71b6f44140bf908fea9b0
berko01 <berkindundar2001@gmail.com>
2025-02-20 13:55:07 +0300

    hazelcast config fixe

--- account-order-management/src/main/java/com/ramobeko/accountordermanagement/AccountOrderManagementApplication.java
+++ account-order-management/src/main/java/com/ramobeko/accountordermanagement/AccountOrderManagementApplication.java
@@ -1,11 +1,17 @@
 package com.ramobeko.accountordermanagement;
 
+import com.ramobeko.accountordermanagement.config.HazelcastConfig;
 import org.apache.ignite.springdata.repository.config.EnableIgniteRepositories;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.FilterType;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
 @SpringBootApplication
+@ComponentScan(excludeFilters = {
+        @ComponentScan.Filter(type = FilterType.ASSIGNABLE_TYPE, classes = HazelcastConfig.class)
+})
 @EnableIgniteRepositories(basePackages = "com.ramobeko.accountordermanagement.repository.ignite")
 @EnableJpaRepositories(basePackages = "com.ramobeko.accountordermanagement.repository.oracle")
 public class AccountOrderManagementApplication {
