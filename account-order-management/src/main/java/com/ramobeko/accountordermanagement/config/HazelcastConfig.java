b3a9d1bb11e3d87f64130d11ba907cc3a2be6591
berko01 <berkindundar2001@gmail.com>
2025-02-20 13:55:07 +0300

    hazelcast config fixe

--- account-order-management/src/main/java/com/ramobeko/accountordermanagement/config/HazelcastConfig.java
+++ account-order-management/src/main/java/com/ramobeko/accountordermanagement/config/HazelcastConfig.java
@@ -3,35 +3,25 @@ package com.ramobeko.accountordermanagement.config;
 import com.hazelcast.config.*;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Primary;
 
 @Configuration
+@Primary  // Eğer birden fazla varsa, bunu öncelikli yapar.
 public class HazelcastConfig {
 
     @Bean
     public Config hazelcastConfig() {
         Config config = new Config();
-        config.setClusterName("hazelcast-cluster"); // Cluster adı
+        config.setClusterName("hazelcast-cluster");
 
-        // Ağ yapılandırması
-        NetworkConfig networkConfig = config.getNetworkConfig();
-        JoinConfig joinConfig = networkConfig.getJoin();
-
-        // Multicast kapalı, çünkü Docker ortamında TCP/IP kullanıyoruz
+        JoinConfig joinConfig = config.getNetworkConfig().getJoin();
         joinConfig.getMulticastConfig().setEnabled(false);
-
-        // Hazelcast düğümlerine bağlan (Docker'daki container isimleri)
-        joinConfig.getTcpIpConfig()
-                .setEnabled(true)
+        joinConfig.getTcpIpConfig().setEnabled(true)
                 .addMember("hazelcast-node-1:5701")
                 .addMember("hazelcast-node-2:5701");
-
-        // Cache yapılandırması
-        config.addMapConfig(
-                new MapConfig()
-                        .setName("customerCache")  // Cache adı
-                        .setTimeToLiveSeconds(600) // 10 dakika sonra temizlensin
-        );
 
         return config;
     }
 }
+
+
