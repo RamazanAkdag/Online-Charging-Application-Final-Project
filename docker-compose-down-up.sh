87ca6f2b9fa70e68cffbdb46ca95b25818ad38ea
berko01 <berkindundar2001@gmail.com>
2025-02-21 12:27:43 +0300

    compose updated

--- docker-compose-down-up.sh
+++ docker-compose-down-up.sh
@@ -1,7 +1,7 @@
 
-docker-compose -f docker-compose.yml -f hazelcast-compose.yml -f ignite-compose.yaml down
+sudo docker-compose -f docker-compose.yml -f hazelcast-compose.yml -f ignite-compose.yaml down
 
-docker-compose -f ignite-compose.yaml up -d
+sudo docker-compose -f ignite-compose.yaml up -d
 
-docker-compose -f hazelcast-compose.yml up -d
+sudo docker-compose -f hazelcast-compose.yml up -d
 
