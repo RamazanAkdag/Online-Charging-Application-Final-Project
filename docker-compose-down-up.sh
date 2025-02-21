b7a232a8e0f723cd564361518573be42fc8f414d
berko01 <berkindundar2001@gmail.com>
2025-02-21 09:11:31 +0300

    composes renewed

--- docker-compose-down-up.sh
+++ docker-compose-down-up.sh
@@ -1,6 +1,3 @@
-docker-compose -f docker-compose.yml down
-docker-compose -f hazelcast-compose.yml down
-docker-compose -f ignite-compose.yaml down
 
 docker-compose -f docker-compose.yml -f hazelcast-compose.yml -f ignite-compose.yaml down
 
