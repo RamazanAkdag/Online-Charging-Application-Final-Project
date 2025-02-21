25ce85965d7f3cfa61823ee90dac2968e9124ff8
berko01 <berkindundar2001@gmail.com>
2025-02-21 13:49:17 +0300

    hazelcast compose update

--- docker-compose-down-up.sh
+++ docker-compose-down-up.sh
@@ -1,5 +1,11 @@
 
-sudo docker-compose -f docker-compose.yml -f hazelcast-compose.yml -f ignite-compose.yaml down
+
+docker network prune -f
+docker container prune -f
+docker volume prune -f
+
+
+sudo docker-compose -f docker-compose.yml -f hazelcast-compose.yml -f ignite-compose.yaml down --remove-or
 
 sudo docker-compose -f ignite-compose.yaml up -d
 
