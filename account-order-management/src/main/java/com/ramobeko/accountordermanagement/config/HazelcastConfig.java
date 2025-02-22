504b528c1d3250bd948bffecf9e0c7814907b8d9
berko01 <berkindundar2001@gmail.com>
2025-02-22 22:52:48 +0300

    client test

--- account-order-management/src/main/java/com/ramobeko/accountordermanagement/config/HazelcastConfig.java
+++ account-order-management/src/main/java/com/ramobeko/accountordermanagement/config/HazelcastConfig.java
@@ -5,16 +5,33 @@ import com.hazelcast.client.config.ClientConfig;
 import com.hazelcast.core.HazelcastInstance;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 @Configuration
 public class HazelcastConfig {
+    private static final Logger logger = LoggerFactory.getLogger(HazelcastConfig.class);
 
     @Bean
     public HazelcastInstance hazelcastClient() {
         ClientConfig clientConfig = new ClientConfig();
-        clientConfig.setClusterName("hazelcast-cluster"); // Küme adı
-        clientConfig.getNetworkConfig().addAddress("127.0.0.1:5701", "127.0.0.1:5702"); // Düğümler
+        clientConfig.setClusterName("hazelcast-cluster");
 
-        return HazelcastClient.newHazelcastClient(clientConfig);
+        // Küme üyelerini IP olarak tanımla
+        clientConfig.getNetworkConfig()
+                .addAddress("18.158.110.143:5701", "18.158.110.143:5702")
+                .setSmartRouting(true)  // Akıllı yönlendirme
+                .setRedoOperation(true) // Hata durumunda tekrar deneme
+                .setConnectionTimeout(5000); // Bağlantı zaman aşımı (5 saniye)
+
+        try {
+            logger.info("Hazelcast client başlatılıyor...");
+            HazelcastInstance client = HazelcastClient.newHazelcastClient(clientConfig);
+            logger.info("Hazelcast client başarıyla bağlandı.");
+            return client;
+        } catch (Exception e) {
+            logger.error("Hazelcast client başlatılırken hata oluştu!", e);
+            throw new RuntimeException("Hazelcast client başlatılamadı.", e);
+        }
     }
 }
