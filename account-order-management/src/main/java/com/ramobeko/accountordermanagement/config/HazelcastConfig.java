82b034292447d588100769195da20525064de800
berko01 <berkindundar2001@gmail.com>
2025-02-22 10:12:47 +0300

    client test removed

--- account-order-management/src/main/java/com/ramobeko/accountordermanagement/config/HazelcastConfig.java
+++ account-order-management/src/main/java/com/ramobeko/accountordermanagement/config/HazelcastConfig.java
@@ -1,7 +1,14 @@
 package com.ramobeko.accountordermanagement.config;
 
-import com.hazelcast.client.config.ClientConfig;
-import com.hazelcast.client.HazelcastClient;
+import com.hazelcast.config.*;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Primary;
+
+import com.hazelcast.config.Config;
+import com.hazelcast.config.JoinConfig;
+import com.hazelcast.config.NetworkConfig;
+import com.hazelcast.core.Hazelcast;
 import com.hazelcast.core.HazelcastInstance;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -11,13 +18,18 @@ public class HazelcastConfig {
 
     @Bean
     public HazelcastInstance hazelcastInstance() {
-        ClientConfig clientConfig = new ClientConfig();
-        clientConfig.setClusterName("hazelcast-cluster");
+        Config config = new Config();
+        config.setClusterName("hazelcast-cluster");
+        config.getJetConfig().setEnabled(true);//ramo
 
-        // Bağlanılacak Hazelcast düğümlerini tanımlayın
-        clientConfig.getNetworkConfig()
-                .addAddress("127.0.0.1:5701", "127.0.0.1:5702");
+        // Network yapılandırması
+        NetworkConfig networkConfig = config.getNetworkConfig();
+        JoinConfig joinConfig = networkConfig.getJoin();
+        joinConfig.getMulticastConfig().setEnabled(false);
+        joinConfig.getTcpIpConfig().setEnabled(true)
+                .addMember("127.0.0.1:5701")
+                .addMember("127.0.0.1:5702");
 
-        return HazelcastClient.newHazelcastClient(clientConfig);
+        return Hazelcast.newHazelcastInstance(config);
     }
 }
