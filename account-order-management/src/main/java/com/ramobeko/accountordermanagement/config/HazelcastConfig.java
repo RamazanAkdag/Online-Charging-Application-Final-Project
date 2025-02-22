d8e3a15c7b3e0a445b1e1774f61672248fad4024
berko01 <berkindundar2001@gmail.com>
2025-02-22 23:18:11 +0300

    commit

--- account-order-management/src/main/java/com/ramobeko/accountordermanagement/config/HazelcastConfig.java
+++ account-order-management/src/main/java/com/ramobeko/accountordermanagement/config/HazelcastConfig.java
@@ -1,37 +1,35 @@
 package com.ramobeko.accountordermanagement.config;
 
-import com.hazelcast.client.HazelcastClient;
-import com.hazelcast.client.config.ClientConfig;
+import com.hazelcast.config.*;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Primary;
+
+import com.hazelcast.config.Config;
+import com.hazelcast.config.JoinConfig;
+import com.hazelcast.config.NetworkConfig;
+import com.hazelcast.core.Hazelcast;
 import com.hazelcast.core.HazelcastInstance;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
 
 @Configuration
 public class HazelcastConfig {
-    private static final Logger logger = LoggerFactory.getLogger(HazelcastConfig.class);
 
     @Bean
-    public HazelcastInstance hazelcastClient() {
-        ClientConfig clientConfig = new ClientConfig();
-        clientConfig.setClusterName("hazelcast-cluster");
+    public HazelcastInstance hazelcastInstance() {
+        Config config = new Config();
+        config.setClusterName("hazelcast-cluster");
+        config.getJetConfig().setEnabled(true);//ramo
 
-        // Küme üyelerini IP olarak tanımla
-        clientConfig.getNetworkConfig()
-                .addAddress("18.158.110.143:5701", "18.158.110.143:5702")
-                .setSmartRouting(true)  // Akıllı yönlendirme
-                .setRedoOperation(true) // Hata durumunda tekrar deneme
-                .setConnectionTimeout(5000); // Bağlantı zaman aşımı (5 saniye)
+        // Network yapılandırması
+        NetworkConfig networkConfig = config.getNetworkConfig();
+        JoinConfig joinConfig = networkConfig.getJoin();
+        joinConfig.getMulticastConfig().setEnabled(false);
+        joinConfig.getTcpIpConfig().setEnabled(true)
+                .addMember("127.0.0.1:5701")
+                .addMember("127.0.0.1:5702");
 
-        try {
-            logger.info("Hazelcast client başlatılıyor...");
-            HazelcastInstance client = HazelcastClient.newHazelcastClient(clientConfig);
-            logger.info("Hazelcast client başarıyla bağlandı.");
-            return client;
-        } catch (Exception e) {
-            logger.error("Hazelcast client başlatılırken hata oluştu!", e);
-            throw new RuntimeException("Hazelcast client başlatılamadı.", e);
-        }
+        return Hazelcast.newHazelcastInstance(config);
     }
 }
