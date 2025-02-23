b236a79b15ee56b95722b6451201715dca317f73
berko01 <berkindundar2001@gmail.com>
2025-02-23 13:43:56 +0300

    ocs started

--- online-charging-system/src/main/java/org/example/onlinechargingsystem/config/IgniteConfig.java
+++ online-charging-system/src/main/java/org/example/onlinechargingsystem/config/IgniteConfig.java
@@ -1,2 +1,19 @@
-package org.example.onlinechargingsystem.config;public class IgniteConfig {
+package org.example.onlinechargingsystem.config;
+
+
+import org.apache.ignite.Ignite;
+import org.apache.ignite.Ignition;
+import org.apache.ignite.configuration.IgniteConfiguration;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class IgniteConfig {
+    @Bean
+    public Ignite igniteInstance() {
+        IgniteConfiguration cfg = new IgniteConfiguration();
+        cfg.setPeerClassLoadingEnabled(true);
+        return Ignition.start(cfg);
+    }
 }
+
