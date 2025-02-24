a8a7033c91f4f53fcfba417b47b90142c011b7c6
berko01 <berkindundar2001@gmail.com>
2025-02-24 15:59:56 +0300

    ocs error needs tests

--- online-charging-system/src/main/java/org/example/onlinechargingsystem/OnlineChargingSystemApplication.java
+++ online-charging-system/src/main/java/org/example/onlinechargingsystem/OnlineChargingSystemApplication.java
@@ -1,9 +1,11 @@
 package org.example.onlinechargingsystem;
 
+import org.apache.ignite.springdata.repository.config.EnableIgniteRepositories;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
+@EnableIgniteRepositories(basePackages = "com.ramobeko.accountordermanagement.repository.ignite")
 public class OnlineChargingSystemApplication {
 
 	public static void main(String[] args) {
