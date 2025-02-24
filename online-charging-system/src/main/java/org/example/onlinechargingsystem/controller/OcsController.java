8c0b655d6d6272075ae7e53fcd5c4cbbb8f9d685
berko01 <berkindundar2001@gmail.com>
2025-02-24 18:25:32 +0300

    ocs error needs tests

---
+++ online-charging-system/src/main/java/org/example/onlinechargingsystem/controller/OcsController.java
@@ -0,0 +1,29 @@
+package org.example.onlinechargingsystem.controller;
+
+
+import com.ramobeko.akka.Command;
+import akka.actor.typed.ActorRef;
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequestMapping("/ocs")
+public class OcsController {
+    private final ActorRef<Command.UsageData> ocsRouter;
+
+    public OcsController(ActorRef<Command.UsageData> ocsRouter) {
+        this.ocsRouter = ocsRouter;
+    }
+
+    @PostMapping("/usage")
+    public ResponseEntity<String> processUsage(@RequestBody Command.UsageData data) {
+        // Akka Router'a mesaj gönder
+        ocsRouter.tell(new Command.UsageData(data.getUserId(), data.getServiceType(), data.getUsageAmount()));
+
+
+        return ResponseEntity.ok("Usage data sent to OCS successfully.");
+    }
+}
+
+
