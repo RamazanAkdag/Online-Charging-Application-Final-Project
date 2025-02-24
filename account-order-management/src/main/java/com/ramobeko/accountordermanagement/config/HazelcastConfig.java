f9cdc7b987957c75570cbd6491ad84477dad6ad5
berko01 <berkindundar2001@gmail.com>
2025-02-24 19:44:48 +0300

    ocs nearly completed

--- account-order-management/src/main/java/com/ramobeko/accountordermanagement/config/HazelcastConfig.java
+++ account-order-management/src/main/java/com/ramobeko/accountordermanagement/config/HazelcastConfig.java
@@ -1,25 +1,32 @@
 package com.ramobeko.accountordermanagement.config;
 
-import com.hazelcast.client.HazelcastClient;
-import com.hazelcast.client.config.ClientConfig;
-import com.hazelcast.client.config.ClientNetworkConfig;
-import com.hazelcast.core.HazelcastInstance;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+
+import com.hazelcast.config.Config;
+import com.hazelcast.config.JoinConfig;
+import com.hazelcast.config.NetworkConfig;
+import com.hazelcast.core.Hazelcast;
+import com.hazelcast.core.HazelcastInstance;
 
 @Configuration
 public class HazelcastConfig {
 
     @Bean
     public HazelcastInstance hazelcastInstance() {
-        ClientConfig clientConfig = new ClientConfig();
-        clientConfig.setClusterName("hazelcast-cluster");
+        Config config = new Config();
+        config.setClusterName("hazelcast-cluster");
+        config.getJetConfig().setEnabled(true);//ramo
 
-        // TCP/IP bağlantı ayarları
-        ClientNetworkConfig networkConfig = clientConfig.getNetworkConfig();
-        networkConfig.addAddress("18.158.110.143:5701", "18.158.110.143:5702");
-        networkConfig.setConnectionTimeout(5000); // 5 saniye timeout ayarla
+        // Network yapılandırması
+        NetworkConfig networkConfig = config.getNetworkConfig();
+        JoinConfig joinConfig = networkConfig.getJoin();
+        joinConfig.getMulticastConfig().setEnabled(false);
+        joinConfig.getTcpIpConfig().setEnabled(true)
+                .addMember("18.158.110.143:5701")
+                .addMember("18.158.110.143:5702");
 
-        return HazelcastClient.newHazelcastClient(clientConfig);
+        return Hazelcast.newHazelcastInstance(config);
     }
 }
+
