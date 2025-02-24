dd1251f8212c1cc3a0e87f6cc604f7fab39afe3c
berko01 <berkindundar2001@gmail.com>
2025-02-24 17:51:03 +0300

    ocs error needs tests

--- account-order-management/src/main/java/com/ramobeko/accountordermanagement/repository/ignite/IgniteCustomerRepository.java
+++ account-order-management/src/main/java/com/ramobeko/accountordermanagement/repository/ignite/IgniteCustomerRepository.java
@@ -1,20 +1,10 @@
 package com.ramobeko.accountordermanagement.repository.ignite;
 
 import com.ramobeko.accountordermanagement.model.entity.ignite.IgniteCustomer;
-import com.ramobeko.accountordermanagement.model.entity.oracle.OracleCustomer;
-import org.apache.ignite.IgniteCache;
 import org.apache.ignite.springdata.repository.IgniteRepository;
 import org.apache.ignite.springdata.repository.config.RepositoryConfig;
-import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
-import javax.cache.expiry.ExpiryPolicy;
-import java.io.Serializable;
-
-import org.apache.ignite.springdata.repository.IgniteRepository;
-import org.apache.ignite.springdata.repository.config.RepositoryConfig;
-import org.apache.ignite.IgniteCache;
-import org.springframework.stereotype.Repository;
 
 @Repository
 @RepositoryConfig(cacheName = "CustomerCache", autoCreateCache = true)
--- online-charging-system/src/main/java/org/example/onlinechargingsystem/repository/ignite/IgniteBalanceRepository.java
+++ online-charging-system/src/main/java/org/example/onlinechargingsystem/repository/ignite/IgniteBalanceRepository.java
@@ -1,14 +1,11 @@
-package org.example.onlinechargingsystem.repository.Ignite;
+package org.example.onlinechargingsystem.repository.ignite;
 
 import org.apache.ignite.springdata.repository.IgniteRepository;
 import org.apache.ignite.springdata.repository.config.RepositoryConfig;
 import org.example.onlinechargingsystem.model.entity.Balance;
 import org.springframework.stereotype.Repository;
-import java.util.Optional;
 
 @Repository
 @RepositoryConfig(cacheName = "BalanceCache", autoCreateCache = true)
 public interface IgniteBalanceRepository extends IgniteRepository<Balance, Long> {
-    Optional<Balance> findById(Long id);
 }
-
