5dd480e8a0adcd8a4b0d1e027e658d19986100d8
berko01 <berkindundar2001@gmail.com>
2025-02-25 20:51:20 +0300

    hazelcast configure needs test

--- account-order-management/src/main/java/com/ramobeko/accountordermanagement/config/HazelcastConfig.java
+++ account-order-management/src/main/java/com/ramobeko/accountordermanagement/config/HazelcastConfig.java
@@ -8,7 +8,27 @@ import com.hazelcast.config.JoinConfig;
 import com.hazelcast.config.NetworkConfig;
 import com.hazelcast.core.Hazelcast;
 import com.hazelcast.core.HazelcastInstance;
+import com.hazelcast.client.HazelcastClient;
+import com.hazelcast.client.config.ClientConfig;
+import com.hazelcast.core.HazelcastInstance;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
 
+@Configuration
+public class HazelcastConfig {
+
+    @Bean
+    public HazelcastInstance hazelcastClient() {
+        ClientConfig clientConfig = new ClientConfig();
+        clientConfig.setClusterName("hazelcast-cluster");
+        clientConfig.getNetworkConfig().addAddress("127.0.0.1:5701");
+
+        return HazelcastClient.newHazelcastClient(clientConfig);
+    }
+}
+
+
+/*
 @Configuration
 public class HazelcastConfig {
 
@@ -31,26 +51,5 @@ public class HazelcastConfig {
 
 
 }
-
-/*
-import com.hazelcast.client.HazelcastClient;
-import com.hazelcast.client.config.ClientConfig;
-import com.hazelcast.core.HazelcastInstance;
-import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.Configuration;
-
-@Configuration
-public class HazelcastConfig {
-
-    @Bean
-    public HazelcastInstance hazelcastClient() {
-        ClientConfig clientConfig = new ClientConfig();
-        clientConfig.setClusterName("hazelcast-cluster");
-        clientConfig.getNetworkConfig().addAddress("127.0.0.1:5701");
-
-        return HazelcastClient.newHazelcastClient(clientConfig);
-    }
-}
-
  */
 
