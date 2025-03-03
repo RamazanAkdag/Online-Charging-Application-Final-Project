a5feeedea694d4d1979d0d703dc8fcea14314df6
berko01 <berkindundar2001@gmail.com>
2025-03-03 19:08:51 +0300

    some configs added

--- online-charging-system/src/main/java/org/example/onlinechargingsystem/config/KafkaConfig.java
+++ online-charging-system/src/main/java/org/example/onlinechargingsystem/config/KafkaConfig.java
@@ -3,6 +3,7 @@ package org.example.onlinechargingsystem.config;
 import org.apache.kafka.clients.producer.ProducerConfig;
 import org.apache.kafka.common.serialization.StringSerializer;
 import org.example.onlinechargingsystem.util.KafkaMessageSerializer;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.kafka.core.DefaultKafkaProducerFactory;
@@ -10,22 +11,25 @@ import org.springframework.kafka.core.KafkaTemplate;
 import org.springframework.kafka.core.ProducerFactory;
 import java.util.HashMap;
 import java.util.Map;
-import com.ramobeko.kafka.ABMFKafkaMessage; // 🔹 KafkaMessage import et
+import com.ramobeko.kafka.ABMFKafkaMessage;
 
 @Configuration
 public class KafkaConfig {
 
+    @Value("${spring.kafka.bootstrap-servers}")
+    private String bootstrapServers;
+
     @Bean
-    public ProducerFactory<String, ABMFKafkaMessage> producerFactory() { // 🔹 Burada KafkaMessage kullanıyoruz
+    public ProducerFactory<String, ABMFKafkaMessage> producerFactory() {
         Map<String, Object> configProps = new HashMap<>();
-        configProps.put(ProducerConfig.BOOTSTRAP_SERVERS_CONFIG, "localhost:9092"); // Kafka broker adresi
+        configProps.put(ProducerConfig.BOOTSTRAP_SERVERS_CONFIG, bootstrapServers);
         configProps.put(ProducerConfig.KEY_SERIALIZER_CLASS_CONFIG, StringSerializer.class);
-        configProps.put(ProducerConfig.VALUE_SERIALIZER_CLASS_CONFIG, KafkaMessageSerializer.class); // 🔹 Özel Serializer eklendi
+        configProps.put(ProducerConfig.VALUE_SERIALIZER_CLASS_CONFIG, KafkaMessageSerializer.class);
         return new DefaultKafkaProducerFactory<>(configProps);
     }
 
     @Bean
-    public KafkaTemplate<String, ABMFKafkaMessage> kafkaTemplate() { // 🔹 Burada KafkaMessage tipiyle değiştirildi
+    public KafkaTemplate<String, ABMFKafkaMessage> kafkaTemplate() {
         return new KafkaTemplate<>(producerFactory());
     }
 }
