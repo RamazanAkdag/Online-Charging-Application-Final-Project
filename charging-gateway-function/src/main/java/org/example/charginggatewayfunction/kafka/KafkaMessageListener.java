35acdb493edee638fb7833b3a0189aed42b9ebff
RamazanAkdag <akdagramazan586@gmail.com>
2025-03-12 16:40:22 +0300

    CGF - cgf working properly, kafdrop added

--- charging-gateway-function/src/main/java/org/example/charginggatewayfunction/kafka/KafkaMessageListener.java
+++ charging-gateway-function/src/main/java/org/example/charginggatewayfunction/kafka/KafkaMessageListener.java
@@ -6,30 +6,27 @@ import org.apache.kafka.clients.consumer.ConsumerRecord;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 import org.example.charginggatewayfunction.service.abstrct.IChargingService;
-import org.springframework.kafka.listener.MessageListener;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.kafka.annotation.KafkaListener;
 import org.springframework.stereotype.Component;
+
 
 @Component
 @RequiredArgsConstructor
-public class KafkaMessageListener implements MessageListener<String, CGFKafkaMessage> {
+public class KafkaMessageListener {
 
     private static final Logger logger = LogManager.getLogger(KafkaMessageListener.class);
 
     private final IChargingService chargingService;
 
-    @Override
-    public void onMessage(ConsumerRecord<String, CGFKafkaMessage> record) {
-        CGFKafkaMessage message = record.value();
+    @KafkaListener(topics = "${spring.kafka.template.default-topic}")
+    public void listen(CGFKafkaMessage message) {
 
-        logger.info("✅ Kafka'dan mesaj alındı. Topic: {}, Partition: {}, Offset: {}, Key: {}, Value: {}",
-                record.topic(), record.partition(), record.offset(), record.key(), message);
+        logger.info("✅ Kafka'dan mesaj alındı. Value: {}", message);
+        chargingService.processCGFMessage(message);
+        logger.info("✅ Mesaj işleme başarıyla tamamlandı: {}", message);
 
-        try {
-            chargingService.processCGFMessage(message);
 
-            logger.info("✅ Mesaj işleme başarıyla tamamlandı: {}", message);
-        } catch (Exception e) {
-            logger.error("❌ Mesaj işlenirken hata oluştu: {}", e.getMessage(), e);
-        }
     }
 }
+
