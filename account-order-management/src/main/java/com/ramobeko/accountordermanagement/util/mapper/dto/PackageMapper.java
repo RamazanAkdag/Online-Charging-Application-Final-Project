7e27c32ea61be47e8c3a086807ef5af7f6a368b7
berko01 <berkindundar2001@gmail.com>
2025-04-25 22:46:37 +0300

    mapper modules updated

--- account-order-management/src/main/java/com/ramobeko/accountordermanagement/util/mapper/dto/PackageMapper.java
+++ account-order-management/src/main/java/com/ramobeko/accountordermanagement/util/mapper/dto/PackageMapper.java
@@ -1,14 +1,12 @@
-package com.ramobeko.accountordermanagement.util.mapper.oracle;
+package com.ramobeko.accountordermanagement.util.mapper.dto;
 
 import com.ramobeko.accountordermanagement.model.dto.OraclePackageDTO;
 import com.ramobeko.accountordermanagement.model.entity.oracle.OraclePackage;
 
+public class PackageMapper {
 
-public class OraclePackageMapper {
     public static OraclePackageDTO toDTO(OraclePackage oraclePackage) {
-        if (oraclePackage == null) {
-            return null;
-        }
+        if (oraclePackage == null) return null;
         return new OraclePackageDTO(
                 oraclePackage.getId(),
                 oraclePackage.getName(),
@@ -20,9 +18,7 @@ public class OraclePackageMapper {
     }
 
     public static OraclePackage toEntity(OraclePackageDTO dto) {
-        if (dto == null) {
-            return null;
-        }
+        if (dto == null) return null;
         OraclePackage oraclePackage = new OraclePackage();
         oraclePackage.setId(dto.getId());
         oraclePackage.setName(dto.getName());
@@ -33,3 +29,4 @@ public class OraclePackageMapper {
         return oraclePackage;
     }
 }
+
