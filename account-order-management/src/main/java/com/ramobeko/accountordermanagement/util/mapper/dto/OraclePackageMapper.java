8d76a7aa8e1de1c6b9c64440c26c77ed234310dc
berko01 <berkindundar2001@gmail.com>
2025-05-06 12:38:53 +0300

    mappers refactored

--- account-order-management/src/main/java/com/ramobeko/accountordermanagement/util/mapper/dto/OraclePackageMapper.java
+++ account-order-management/src/main/java/com/ramobeko/accountordermanagement/util/mapper/dto/OraclePackageMapper.java
@@ -1,4 +1,19 @@
 package com.ramobeko.accountordermanagement.util.mapper.dto;
 
-public class OraclePackageMapper {
+import com.ramobeko.accountordermanagement.model.dto.OraclePackageDTO;
+import com.ramobeko.accountordermanagement.model.entity.oracle.OraclePackage;
+import org.mapstruct.BeanMapping;
+import org.mapstruct.Mapper;
+import org.mapstruct.MappingTarget;
+import org.mapstruct.NullValuePropertyMappingStrategy;
+
+@Mapper(componentModel = "spring")
+public interface OraclePackageMapper {
+
+    OraclePackage toEntity(OraclePackageDTO dto);
+
+    OraclePackageDTO toDto(OraclePackage entity);
+
+    @BeanMapping(nullValuePropertyMappingStrategy = NullValuePropertyMappingStrategy.IGNORE)
+    void updateFromDto(OraclePackageDTO dto, @MappingTarget OraclePackage entity);
 }
--- account-order-management/src/main/java/com/ramobeko/accountordermanagement/util/mapper/oracle/OracleSubscriberMapper.java
+++ account-order-management/src/main/java/com/ramobeko/accountordermanagement/util/mapper/oracle/OracleSubscriberMapper.java
@@ -1,4 +1,48 @@
 package com.ramobeko.accountordermanagement.util.mapper.oracle;
 
-public class OracleSubscriberMapper {
+import com.ramobeko.accountordermanagement.model.dto.request.SubscriberRequest;
+import com.ramobeko.accountordermanagement.model.dto.request.SubscriberUpdateRequest;
+import com.ramobeko.accountordermanagement.model.entity.oracle.OracleBalance;
+import com.ramobeko.accountordermanagement.model.entity.oracle.OracleCustomer;
+import com.ramobeko.accountordermanagement.model.entity.oracle.OraclePackage;
+import com.ramobeko.accountordermanagement.model.shared.OracleSubscriber;
+import org.mapstruct.BeanMapping;
+import org.mapstruct.Mapper;
+import org.mapstruct.MappingTarget;
+import org.mapstruct.NullValuePropertyMappingStrategy;
+
+import java.util.Date;
+
+@Mapper(componentModel = "spring")
+public interface OracleSubscriberMapper {
+
+    @BeanMapping(nullValuePropertyMappingStrategy = NullValuePropertyMappingStrategy.IGNORE)
+    void updateFromDto(SubscriberUpdateRequest dto, @MappingTarget OracleSubscriber subscriber);
+
+    // Subscriber oluşturma özel mantık içerdiğinden default method
+    default OracleSubscriber create(OracleCustomer customer,
+                                    OraclePackage packagePlan,
+                                    SubscriberRequest request,
+                                    String phoneNumber) {
+        OracleSubscriber subscriber = new OracleSubscriber();
+        subscriber.setCustomer(customer);
+        subscriber.setPackagePlan(packagePlan);
+        subscriber.setPhoneNumber(phoneNumber);
+        subscriber.setStartDate(request.getStartDate() != null ? request.getStartDate() : new Date());
+        subscriber.setStatus("ACTIVE");
+        return subscriber;
+    }
+
+    default OracleBalance createBalance(OracleSubscriber subscriber, OraclePackage packagePlan) {
+        OracleBalance balance = new OracleBalance();
+        balance.setSubscriber(subscriber);
+        balance.setPackagePlan(packagePlan);
+        balance.setLevelMinutes(packagePlan.getAmountMinutes());
+        balance.setLevelSms(packagePlan.getAmountSms());
+        balance.setLevelData(packagePlan.getAmountData());
+        balance.setStartDate(new Date());
+        balance.setEndDate(null);
+        return balance;
+    }
 }
+
