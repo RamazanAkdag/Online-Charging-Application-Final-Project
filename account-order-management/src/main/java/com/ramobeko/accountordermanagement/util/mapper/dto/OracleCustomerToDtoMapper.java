53e8ae59a3a3bfc139f1c2d77629d1872b59c899
berko01 <berkindundar2001@gmail.com>
2025-05-06 12:38:53 +0300

    mappers refactored

--- account-order-management/src/main/java/com/ramobeko/accountordermanagement/util/mapper/dto/OracleCustomerToDtoMapper.java
+++ account-order-management/src/main/java/com/ramobeko/accountordermanagement/util/mapper/dto/OracleCustomerToDtoMapper.java
@@ -1,4 +1,12 @@
 package com.ramobeko.accountordermanagement.util.mapper.dto;
 
-public class OracleCustomerToDtoMapper {
+import com.ramobeko.accountordermanagement.model.dto.OracleCustomerDTO;
+import com.ramobeko.accountordermanagement.model.entity.oracle.OracleCustomer;
+import org.mapstruct.Mapper;
+
+@Mapper(componentModel = "spring")
+public interface OracleCustomerToDtoMapper {
+
+    OracleCustomerDTO toDto(OracleCustomer customer);
 }
+
