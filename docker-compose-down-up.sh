d5748d7809fd9d2b4457c64f0ee5daf505655444
berko01 <berkindundar2001@gmail.com>
2025-05-06 17:27:19 +0300

    dockerization started

--- docker-compose-down-up.sh
+++
@@ -1,8 +0,0 @@
-sudo docker-compose -f hazelcast-compose.yml -f ignite-compose.yml -f kafka-compose.yml down --remove-or
-
-sudo docker-compose -f ignite-compose.yml up -d
-
-sudo docker-compose -f hazelcast-compose.yml up -d
-
-sudo docker-compose -f kafka-compose.yml up -d
-
