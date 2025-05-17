e19d709c09be6a0b85fbed60985d7c44a46e0b58
berko01 <berkindundar2001@gmail.com>
2025-05-17 12:53:00 +0300

    igw is ready

--- terraform-and-ansible/modules/network/ main.tf
+++ terraform-and-ansible/modules/network/ main.tf
@@ -1,12 +1,10 @@
 resource "aws_vpc" "this" {
   cidr_block           = var.vpc_cidr
   enable_dns_hostnames = true
-  tags = { Name = var.name }
-}
 
-resource "aws_internet_gateway" "this" {
-  vpc_id = aws_vpc.this.id
-  tags = { Name = "${var.name}-igw" }
+  tags = {
+    Name = var.name
+  }
 }
 
 resource "aws_subnet" "public" {
@@ -14,16 +12,31 @@ resource "aws_subnet" "public" {
   cidr_block              = var.public_subnet_cidr
   map_public_ip_on_launch = true
   availability_zone       = var.az
-  tags = { Name = "${var.name}-public-subnet" }
+
+  tags = {
+    Name = "${var.name}-public-subnet"
+  }
+}
+
+resource "aws_internet_gateway" "this" {
+  vpc_id = aws_vpc.this.id
+
+  tags = {
+    Name = "${var.name}-igw"
+  }
 }
 
 resource "aws_route_table" "public" {
   vpc_id = aws_vpc.this.id
+
   route {
     cidr_block = "0.0.0.0/0"
     gateway_id = aws_internet_gateway.this.id
   }
-  tags = { Name = "${var.name}-public-rt" }
+
+  tags = {
+    Name = "${var.name}-public-rt"
+  }
 }
 
 resource "aws_route_table_association" "public" {
