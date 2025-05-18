72419aee6304b9dcda90175c22d6ad5f380a5269
berko01 <berkindundar2001@gmail.com>
2025-05-18 21:20:25 +0300

    terraform is ready for deployment

--- terraform/environments/dev/outputs.tf
+++
@@ -1,9 +0,0 @@
-output "all_ips" {
-  value = {
-    sonarqube   = module.sonarqube.public_ip
-    nexus       = module.nexus.public_ip
-    k8s_master  = module.k8s_master.public_ip
-    k8s_slave_1 = module.k8s_slave_1.public_ip
-    k8s_slave_2 = module.k8s_slave_2.public_ip
-  }
-}
--- terraform/environments/dev/variables.tf
+++
@@ -1,5 +0,0 @@
-variable "aws_region" {
-  description = "AWS region to deploy resources into"
-  type        = string
-  default     = "eu-central-1"
-}
