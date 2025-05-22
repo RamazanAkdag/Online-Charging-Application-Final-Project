d37d8f7f3a442bdd54ab4998ad9b948134ee4bfc
akdagramazan586@gmail.com <akdagramazan586@gmail.com>
2025-05-22 14:50:53 +0300

    ANSIBLE - installed K8s and jenkins machines

--- ansible/inventory_generator.py
+++ ansible/inventory_generator.py
@@ -1,6 +1,7 @@
 #!/usr/bin/env python3
 
 import json
+import sys
 
 hosts = {}
 with open("ec2_ids", "r") as f:
@@ -10,17 +11,30 @@ with open("ec2_ids", "r") as f:
             hosts[k.strip()] = v.strip()
 
 inventory = {
-    "all": {
-        "children": {
-            "nexus": {"hosts": [hosts["nexus"]]},
-            "sonarqube": {"hosts": [hosts["sonarqube"]]},
-            "k8s_master": {"hosts": [hosts["k8s_master"]]},
-            "k8s_slaves": {
-                "hosts": [hosts["k8s_slave_1"], hosts["k8s_slave_2"]]
-            },
-        }
+    "_meta": {
+        "hostvars": {}
+    },
+    "nexus": {
+        "hosts": [hosts["nexus"]]
+    },
+    "sonarqube": {
+        "hosts": [hosts["sonarqube"]]
+    },
+    "k8s_master": {
+        "hosts": [hosts["k8s_master"]]
+    },
+    "k8s_slaves": {
+        "hosts": [hosts["k8s_slave_1"], hosts["k8s_slave_2"]]
+    },
+    "jenkins": {
+        "hosts": [hosts["jenkins"]]
     }
 }
 
-print(json.dumps(inventory, indent=2))
-
+if "--list" in sys.argv:
+    print(json.dumps(inventory, indent=2))
+elif "--host" in sys.argv:
+    print(json.dumps({}))
+else:
+    print("Usage: --list or --host <hostname>", file=sys.stderr)
+    sys.exit(1)
